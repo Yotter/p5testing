fe5a98f367177d8dec88bfc0375f283e8bc1cfa5
Yotter <lavamarmax@gmail.com>
2023-12-09 02:09:24 -0800

    Draw smooth curve

--- scripts/bezier.js
+++ scripts/bezier.js
@@ -3,10 +3,9 @@
 // Global variables
 var t; // t-value for animation
 var tPoint; // Point on the curve at t
-var points = []; // Array of control points
+var controlPoints = []; // Array of control points
+var curvePoints = []; // Array of points on the curve
 var guideLines = []; // Array of guide lines
-var drawnPoints = []; // Array of points drawn on the curve
-var finished = false; // Is the curve finished?
 
 // Colors
 var backgroundColor;
@@ -15,9 +14,9 @@ var guideLineColor;
 
 function mousePressed() {
     // Add a new control point
-    points.push(createVector(mouseX, mouseY));
-    drawnPoints = [];
-    finished = false
+    controlPoints.push(createVector(mouseX, mouseY));
+    t = 0;
+    curvePoints = [];
 }
 
 function updateGuideLines() {
@@ -25,26 +24,37 @@ function updateGuideLines() {
     guideLines = [];
 
     // Recursively update guide lines
-    updateGuideLinesHelper(points);
+    updateGuideLinesHelper(controlPoints);
 }
 
-function updateGuideLinesHelper(points) {
-    // update guide lines
-    for (let i = 0; i < points.length - 1; i++) {
-        guideLines.push([points[i], points[i + 1]]);
-    }
-
-    let newPoints = [];
-    for (let i = 0; i < points.length - 1; i++) {
-        let x = lerp(points[i].x, points[i + 1].x, t);
-        let y = lerp(points[i].y, points[i + 1].y, t);
-        newPoints.push(createVector(x, y));
-    }
-    if (newPoints.length > 1) {
-        updateGuideLinesHelper(newPoints);
+function updateGuideLinesHelper(ctrlPoints) {
+    // Base case
+    if (ctrlPoints.length <= 1) {
+        tPoint = ctrlPoints[0];
     } else {
-        tPoint = newPoints[0];
+        // add guide lines
+        for (let i = 0; i < ctrlPoints.length - 1; i++) {
+            guideLines.push([ctrlPoints[i], ctrlPoints[i + 1]]);
+        }
+    
+        // Find points on new lines
+        let newPoints = [];
+        for (let i = 0; i < ctrlPoints.length - 1; i++) {
+            let x = lerp(ctrlPoints[i].x, ctrlPoints[i + 1].x, t);
+            let y = lerp(ctrlPoints[i].y, ctrlPoints[i + 1].y, t);
+            newPoints.push(createVector(x, y));
+        }
+        updateGuideLinesHelper(newPoints);
     }
+}
+
+function doesNotIncludeVector(array, vector) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].x == vector.x && array[i].y == vector.y) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function setup() {
@@ -52,20 +62,18 @@ function setup() {
     backgroundColor = color("#a4a2a8");
     controlPointColor = color("#c23728");
     guideLineColor = color("#111111");
+    t = 0;
 }
 
 function draw() {
-    // Set t
-    t = frameCount%100 / 100;
-
     // Draw background
     background(backgroundColor);
 
     // Draw control points
-    for (let i = 0; i < points.length; i++) {
+    for (let i = 0; i < controlPoints.length; i++) {
         fill(controlPointColor);
         stroke(0);
-        circle(points[i].x, points[i].y, 10);
+        circle(controlPoints[i].x, controlPoints[i].y, 10);
     }
 
     // Draw guide lines
@@ -75,15 +83,30 @@ function draw() {
         line(guideLines[i][0].x, guideLines[i][0].y, guideLines[i][1].x, guideLines[i][1].y);
     }
 
-    // Draw points on the curve
+    // Draw curve
     if (tPoint) {
-        fill(0);
-        stroke(0);
-        circle(tPoint.x, tPoint.y, 10);
-        drawnPoints.push(tPoint);
-        for (let i = 0; i < drawnPoints.length - 1; i++) {
-            circle(drawnPoints[i].x, drawnPoints[i].y, 10);
+        console.log("t: " + t + ", tPoint: " + tPoint);
+        if (doesNotIncludeVector(curvePoints, tPoint)) {
+            curvePoints.push(tPoint);
         }
+        stroke(255, 0, 0);
+        for (let i = 0; i < curvePoints.length - 1; i++) {
+            line(curvePoints[i].x, curvePoints[i].y, curvePoints[i + 1].x, curvePoints[i + 1].y);
+        }
+    }
+
+    // Draw the last point in curvePoints for debugging
+    // if (curvePoints.length > 0) {
+    //     fill(255,255,0);
+    //     stroke(0);
+    //     circle(curvePoints[curvePoints.length - 1].x, curvePoints[curvePoints.length - 1].y, 10);
+    //     console.log("curvePoints: " + curvePoints[0]);
+    // }
+
+    // Set t
+    t+=0.01;
+    if (t > 1.000001) {
+        t = 0;
     }
 
 }
