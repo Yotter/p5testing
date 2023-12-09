bd0e445ee0de57825c55fa0503183935568adaff
Yotter <lavamarmax@gmail.com>
2023-12-09 02:53:37 -0800

    Add reset button and change speed button

--- scripts/bezier.js
+++ scripts/bezier.js
@@ -1,6 +1,8 @@
 // Bezier curve drawer in p5.js
 
-// Global variables
+// EDITABLE VARIABLES
+let speed = 0.01; // Speed of the animation
+
 var t; // t-value for animation
 var tPoint; // Point on the curve at t
 var controlPoints = []; // Array of control points
@@ -11,13 +13,8 @@ var guideLines = []; // Array of guide lines
 var backgroundColor;
 var controlPointColor;
 var guideLineColor;
+var curveColor;
 
-function mousePressed() {
-    // Add a new control point
-    controlPoints.push(createVector(mouseX, mouseY));
-    t = 0;
-    curvePoints = [];
-}
 
 function updateGuideLines() {
     // Clear guide lines
@@ -57,11 +54,41 @@ function doesNotIncludeVector(array, vector) {
     return true;
 }
 
+function keyPressed() {
+    if (key == 'r') {
+        controlPoints = [];
+        t = 0;
+        curvePoints = [];
+    }
+    // If key == up arrow, increase speed
+    if (keyCode == UP_ARROW) {
+        if (speed * 2 <= 0.01) {
+            speed *= 2;
+            t = 0;
+            curvePoints = [];
+        }
+    } else if (keyCode == DOWN_ARROW) {
+        speed /= 2;
+        t = 0;
+        curvePoints = [];
+    }
+
+}
+
+function mousePressed() {
+    // Add a new control point
+    controlPoints.push(createVector(mouseX, mouseY));
+    t = 0;
+    curvePoints = [];
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    document.title = "Bezier Curve Drawer - Press R to reset, Up/Down to change speed";
+    createCanvas(windowWidth, windowHeight);
     backgroundColor = color("#a4a2a8");
     controlPointColor = color("#c23728");
     guideLineColor = color("#111111");
+    curveColor = controlPointColor;
     t = 0;
 }
 
@@ -73,12 +100,14 @@ function draw() {
     for (let i = 0; i < controlPoints.length; i++) {
         fill(controlPointColor);
         stroke(0);
+        strokeWeight(2);
         circle(controlPoints[i].x, controlPoints[i].y, 10);
     }
 
     // Draw guide lines
     updateGuideLines();
     stroke(guideLineColor);
+    strokeWeight(1);
     for (let i = 0; i < guideLines.length; i++) {
         line(guideLines[i][0].x, guideLines[i][0].y, guideLines[i][1].x, guideLines[i][1].y);
     }
@@ -89,10 +118,19 @@ function draw() {
         if (doesNotIncludeVector(curvePoints, tPoint)) {
             curvePoints.push(tPoint);
         }
-        stroke(255, 0, 0);
+
+        // Draw curve
+        stroke(curveColor);
+        strokeWeight(5);
         for (let i = 0; i < curvePoints.length - 1; i++) {
             line(curvePoints[i].x, curvePoints[i].y, curvePoints[i + 1].x, curvePoints[i + 1].y);
         }
+
+        // Draw tPoint
+        fill(100, 0, 0);
+        stroke(0);
+        strokeWeight(1);
+        circle(tPoint.x, tPoint.y, 10);
     }
 
     // Draw the last point in curvePoints for debugging
@@ -104,7 +142,7 @@ function draw() {
     // }
 
     // Set t
-    t+=0.01;
+    t+=speed;
     if (t > 1.000001) {
         t = 0;
     }
